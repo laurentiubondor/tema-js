9b7978f61106a49d1cc8a72459b8477cb4b9457d
unknown <laurentiu.bondor@gmail.com>
2020-08-10 11:53:53 +0300

    second commit

---
+++ tema.js
@@ -0,0 +1,65 @@
+
+
+//display in the console the numbers from 1 to 20
+
+for ( var i=0; i<=20; i++)  
+console.log(i);
+
+//-------------------------------------------------
+
+
+//display in the console the odd numbers from 1 to 20
+
+//for( var i=1; i<=20; i++)
+//if(i % 2 !==0 )
+//console.log(i);
+
+
+
+//----------------------------------------------------
+
+//compute the sum of the elements of an array and display it in the console
+
+//var sum = 0;
+
+//var array=[4,9,2];
+
+//for (var i=0; i<array.length; i++)
+//{
+
+//sum+=array[i]
+//}
+
+//console.log(sum);
+
+//-------------------------------------------------------------
+
+//compute the maximum of the elements of an array and display it in the console 
+
+//var array=[4,9,2];
+
+//var max= array[0];
+
+//for (var i= 1; i < array.length; i++)
+// {
+
+
+// if (array[i] > max) {
+//   max=array[i];
+//  }
+//  }
+// console.log(max)
+
+//-----------------------------------------------------------------------------------
+
+
+
+//compute how many times a certain element appears in an array
+
+
+//var array = [3, 4, 5, 6, 7, 3, 4, 3, 7, 3];
+//var n = 0;
+//for (i = 0; i < array.length; i++) {
+  // if (array[i] == 3) { n++ }
+//}
+//console.log(n);
